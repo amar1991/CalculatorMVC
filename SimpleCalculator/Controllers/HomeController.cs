0b59ee9c68e4f869beb3ded29a7dc8973cc09880
amar1991 <anitendra@gmail.com>
2017-10-30 10:41:47 +0000

    SimpleCalcMVC

---
+++ SimpleCalculator/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using SimpleCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SimpleCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int number1, int number2, string Operator)
+        {
+            Calculator calc = new Calculator(number1, number2);
+           
+ 
+            if (ModelState.IsValid)
+            {
+                while (Operator != "Select Operator")
+                {
+
+                    if (Operator == "Add")
+                    {
+                        calc.Add();
+                        return View(ViewBag.Outcome = calc.outcome);
+                        
+                    }
+
+                    if (Operator == "Subtract")
+                    {
+                        calc.Subtract();
+                        return View(ViewBag.Outcome = calc.outcome);
+                        
+                    
+                    }
+
+                    if (Operator == "Divide")
+                    {
+                        calc.Divide();
+                        return View(ViewBag.Outcome = calc.outcome);
+                        
+                        
+                    }
+
+                    if (Operator == "Multiply")
+                    {
+                        calc.Multiply();
+                        return View(ViewBag.Outcome = calc.outcome);
+                        
+                        
+                    }
+                
+                }
+            
+            }
+
+            return View();
+        }
+
+
+    }
+}
---
+++ SimpleCalculator/Models/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCalculator.Models
+{
+    public class Calculator
+    {
+        private int number1 { get; set; }
+        private int number2 { get; set; }
+        public dynamic outcome { get; set; }
+        
+
+        public Calculator(int num1, int num2) 
+        {
+            this.number1 = num1;
+            this.number2 = num2;
+        
+        }
+
+
+        public int Add() 
+        {
+            return outcome = number1 + number2;
+        
+        }
+
+        public int Subtract()
+        {
+            return outcome = number1 - number2;
+
+        }
+        public int Multiply()
+        {
+            return outcome = number1 * number2;
+
+        }
+
+        public double Divide()
+        {
+            return (outcome) = number1 / number2;
+
+        }
+
+
+
+
+    }
+}
